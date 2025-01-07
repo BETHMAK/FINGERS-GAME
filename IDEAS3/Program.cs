5a0e15a612011a985754bac8d084457e96176b0b
B3THMAK <132462124+B3THMAK@users.noreply.github.com>
2025-01-07 23:14:08 +0300

    Add project files.

---
+++ IDEAS3/Program.cs
@@ -0,0 +1,19 @@
+﻿
+using System;
+using System.Net.Sockets;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+using System.IO;
+// Program.cs
+static class Program
+{
+    [STAThread]
+    static void Main()
+    {
+        Application.EnableVisualStyles();
+        Application.SetCompatibleTextRenderingDefault(false);
+       
+    }
+}
